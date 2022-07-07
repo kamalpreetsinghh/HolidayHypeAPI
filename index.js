90d125ddb22f5284e6b7ca4ce926290b08fb1d5e
Kamal Preet Singh <kamalpreetsingghh@gmail.com>
2022-07-07 08:58:34 -0400

    Change Port to 3000

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
 
